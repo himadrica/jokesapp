38229327830c16e08c91e4e92c00bf88aacc3653
Himadri Talukder <himadri.talukder@agileasolutions.com>
2020-05-26 16:13:34 -0400

    Added xml configuration for spring bean declarations

--- src/main/java/com/pandit/jokes/jokeapp/JokeappApplication.java
+++ src/main/java/com/pandit/jokes/jokeapp/JokeappApplication.java
@@ -2,8 +2,10 @@ package com.pandit.jokes.jokeapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("classpath:jokes-config.xml") // telling spring that this is xml based config and load all the bean declared in it
 public class JokeappApplication {
 
     public static void main(String[] args) {
